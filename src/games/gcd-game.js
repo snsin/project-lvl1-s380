98124a70c5326e1c16e8e309fa0be21d9b5b2822
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2018-11-17 00:20:39 +0300

    add brain-gcd: logic - src/games/gcd-game.js, starter - src/bin/brain-gcd

---
+++ src/games/gcd-game.js
@@ -0,0 +1,24 @@
+import { play } from '..';
+import intGenerator from '../utils/number-utils';
+
+const description = 'Find the greatest common divisor of given numbers.\n';
+
+const gcd = (num1, num2) => {
+  const iter = (a, b) => {
+    if (b === 0) {
+      return a;
+    }
+    return iter(b, a % b);
+  };
+  return num1 > num2 ? iter(num1, num2) : iter(num2, num1);
+};
+
+const moveGenerator = () => {
+  const a = intGenerator();
+  const b = intGenerator();
+  const question = `${a} ${b}`;
+  const answer = `${gcd(a, b)}`;
+  return { question, answer };
+};
+
+export default () => play({ description, moveGenerator });
