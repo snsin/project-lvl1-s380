60d00096aa8e3caa482adf3007a17b1d1d11d7ad
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2018-11-17 19:39:21 +0300

    fix issues: rename source files in src/game folder, fix import in starters - src/bin

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import start from '../games/even-game';
+import start from '../games/even';
 
 start();
--- src/bin/brain-gcd.js
+++ src/bin/brain-gcd.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import start from '../games/gcd-game';
+import start from '../games/gcd';
 
 start();
